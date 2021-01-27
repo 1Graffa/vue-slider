291d6fd3b0eebdf3d81d32071bb06426acab6cf5
Giuseppe <giusepperaffa1988@gmail.com>
2021-01-27 16:49:27 +0100

    nome + descrizione

--- js/script.js
+++ js/script.js
@@ -8,14 +8,6 @@ const squirtle = "https://projectpokemon.org/images/normal-sprite/squirtle.gif";
 const wartortle = "https://projectpokemon.org/images/normal-sprite/wartortle.gif";
 const blastoise = "https://projectpokemon.org/images/normal-sprite/blastoise.gif";
 const pikachu = "https://projectpokemon.org/images/normal-sprite/pikachu.gif";
-const krabby = "https://projectpokemon.org/images/normal-sprite/krabby.gif";
-const kingler = "https://projectpokemon.org/images/normal-sprite/kingler.gif";
-const dratini = "https://projectpokemon.org/images/normal-sprite/dratini.gif";
-const dragonair = "https://projectpokemon.org/images/normal-sprite/dragonair.gif";
-const dragonite = "https://projectpokemon.org/images/normal-sprite/dragonite.gif";
-const mew = "https://projectpokemon.org/images/normal-sprite/mew.gif";
-const mewtwo = "https://projectpokemon.org/images/normal-sprite/mewtwo.gif";
-
 
 var app = new Vue({
   el: "#container",
@@ -31,14 +23,31 @@ var app = new Vue({
       squirtle,
       wartortle,
       blastoise,
-      pikachu,
-      krabby,
-      kingler,
-      dratini,
-      dragonair,
-      dragonite,
-      mew,
-      mewtwo
+      pikachu
+    ],
+    nomi : [
+      "bulbasaur",
+      "ivysaur",
+      "venusaur",
+      "charmander",
+      "charmeleon",
+      "charizard",
+      "squirtle",
+      "wartortle",
+      "blastoise",
+      "pikachu"
+    ],
+    descrizione: [
+      "Fin dalla nascita questo Pokémon ha sulla schiena un seme che cresce lentamente.",
+      "Il bulbo sulla schiena è cresciuto così tanto da impedirgli di alzarsi in piedi sulle zampe posteriori.",
+      "Il fiore sboccia assorbendo energia solare. Si muove continuamente in cerca di luce.",
+      "Ama le cose calde. Si dice che quando piove gli esca vapore dalla punta della coda.",
+      "Ha un'indole feroce. Usa la coda fiammeggiante come una frusta e lacera l'avversario con gli artigli affilati.",
+      "Sputa fiamme incandescenti in grado di fondere le rocce. A volte causa incendi boschivi.",
+      "Quando ritrae il lungo collo dentro la corazza sputa un vigoroso getto d'acqua.",
+      "È considerato un simbolo di longevità. Se c'è del muschio sul suo guscio, significa che è molto anziano.",
+      "Mette KO gli avversari schiacciandoli sotto il corpo possente. Se è in difficoltà, può ritrarsi nella corazza.",
+      "Più potente è l'energia elettrica prodotta dal Pokémon, più le sacche sulle sue guance sono morbide ed elastiche."
     ],
     intervallo : ""
   },
